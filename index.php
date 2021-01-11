86da39cff42eeaac38b2e9cf8dd762b32124eadb
PROGR WEB - MANHA <PROGR WEB - MANHA@MININT-M6IUDRR>
2021-01-11 12:26:52 -0200

    roubando site

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <title>index</title>
+        <link rel="stylesheet" href="../plugins/bootstrap/css/bootstrap.min.css" />
+    </head>
+    <body>
+        <div class="container">
+
+            <a href="../plugins-1/inicio.php" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">plugins 1</a>
+            <a href="../plugins-2/plug.php" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">plugins 2</a>
+            <a href="../plugins-3/novo.php" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">plugins 3</a>
+            <a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">plugins 5</a>
+            <a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">plugins 6</a>
+
+        </div>
+    </body>
+</html>
---
+++ plugins-1/inicio.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>CodePen - 3D Retro Buttons</title>
+  <link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<div class="wrapper">
+  <div class="header-wrap">
+    <h1>3D Retro Buttons</h1>
+  </div>
+</div>
+
+<br/><br/>
+
+<div class='wrapper'>
+  <div role='button' class='retro-btn'>
+    <a class='btn'> 
+      <span class='btn-inner'>
+        <span class='content-wrapper'>
+          <span class='btn-content'>
+            <span class='btn-content-inner' label='Retro Button'>
+            </span>
+          </span>
+        </span>
+      </span>
+    </a>
+  </div>
+</div>
+
+<div style="display: inline-block; width: 50px"></div>
+
+<div class="wrapper">
+  <button class="loader-button">Loader</button>
+</div>
+
+<br/>
+
+<div class='wrapper'>
+  <div role='button' class='retro-btn lg primary'>
+    <a class='btn'> 
+      <span class='btn-inner'>
+        <span class='content-wrapper'>
+          <span class='btn-content'>
+            <span class='btn-content-inner' label='Large'>
+            </span>
+          </span>
+        </span>
+      </span>
+    </a>
+  </div>
+</div>
+
+<div style="display: inline-block; width: 30px"></div>
+
+<div class='wrapper'>
+  <div role='button' class='retro-btn primary'>
+    <a class='btn'> 
+      <span class='btn-inner'>
+        <span class='content-wrapper'>
+          <span class='btn-content'>
+            <span class='btn-content-inner' label='Medium'>
+            </span>
+          </span>
+        </span>
+      </span>
+    </a>
+  </div>
+</div>
+
+<div style="display: inline-block; width: 30px"></div>
+
+<div class='wrapper'>
+  <div role='button' class='retro-btn sm primary'>
+    <a class='btn'> 
+      <span class='btn-inner'>
+        <span class='content-wrapper'>
+          <span class='btn-content'>
+            <span class='btn-content-inner' label='Small'>
+            </span>
+          </span>
+        </span>
+      </span>
+    </a>
+  </div>
+</div>
+
+<br />
+
+<div class='wrapper'>
+  <div role='button' class='retro-btn primary'>
+    <a class='btn'> 
+      <span class='btn-inner'>
+        <span class='content-wrapper'>
+          <span class='btn-content'>
+            <span class='btn-content-inner' label='Primary'>
+            </span>
+          </span>
+        </span>
+      </span>
+    </a>
+  </div>
+</div>
+
+<div style="display: inline-block; width: 30px"></div>
+
+<div class='wrapper'>
+  <div role='button' class='retro-btn secondary'>
+    <a class='btn'> 
+      <span class='btn-inner'>
+        <span class='content-wrapper'>
+          <span class='btn-content'>
+            <span class='btn-content-inner' label="Secondary">
+            </span>
+          </span>
+        </span>
+      </span>
+    </a>
+  </div>
+</div>
+
+<div style="display: inline-block; width: 30px"></div>
+
+<div class='wrapper'>
+  <div role='button' class='retro-btn danger'>
+    <a class='btn'> 
+      <span class='btn-inner'>
+        <span class='content-wrapper'>
+          <span class='btn-content'>
+            <span class='btn-content-inner' label="Danger">
+            </span>
+          </span>
+        </span>
+      </span>
+    </a>
+  </div>
+</div>
+
+<br />
+
+<div class='wrapper'>
+  <div role='button' class='retro-btn warning'>
+    <a class='btn'> 
+      <span class='btn-inner'>
+        <span class='content-wrapper'>
+          <span class='btn-content'>
+            <span class='btn-content-inner' label="Warning">
+            </span>
+          </span>
+        </span>
+      </span>
+    </a>
+  </div>
+</div>
+
+<div style="display: inline-block; width: 30px"></div>
+
+<div class='wrapper'>
+  <div role='button' class='retro-btn success'>
+    <a class='btn'> 
+      <span class='btn-inner'>
+        <span class='content-wrapper'>
+          <span class='btn-content'>
+            <span class='btn-content-inner' label="Success">
+            </span>
+          </span>
+        </span>
+      </span>
+    </a>
+  </div>
+</div>
+
+<div style="display: inline-block; width: 30px"></div>
+
+<div class='wrapper'>
+  <div role='button' class='retro-btn info'>
+    <a class='btn'> 
+      <span class='btn-inner'>
+        <span class='content-wrapper'>
+          <span class='btn-content'>
+            <span class='btn-content-inner' label="Info">
+            </span>
+          </span>
+        </span>
+      </span>
+    </a>
+  </div>
+</div>
+<!-- partial -->
+<script  src="script.js"></script>
+
+</body>
+</html>
